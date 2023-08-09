6bbb668c4136b83d78e4063269295b06bfe7eb1c
Khalid waleed alrashdi <138435240+5i37@users.noreply.github.com>
2023-08-09 11:52:03 +0400

    Update selecting from first or last or second letter.sql

--- SQLQuery(selecting from first or last or second letter ).sql
+++ SQLQuery(selecting from first or last or second letter ).sql
@@ -6,3 +6,6 @@ where FirstName  like '_s%' ;  -- second letter
 
 select top 5 * from Customer 
 where FirstName  like '%a' ;  -- last letter 
+
+select top 5 * from Customer 
+where FirstName  like '%_s%' ;  --any name that have s letter 
