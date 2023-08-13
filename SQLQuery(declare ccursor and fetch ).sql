a82861f00e251b5812ec2c6b0b2fb6081fef7dec
Khalid waleed alrashdi <138435240+5i37@users.noreply.github.com>
2023-08-13 13:17:18 +0400

    SQLQuery(declare ccursor and fetch ).sql

--- SQLQuery(declare ccursor and fetch ).sql
+++ SQLQuery(declare ccursor and fetch ).sql
@@ -10,9 +10,9 @@ declare @fname nvarchar(40);
 declare @city nvarchar(40); 
 FETCH NEXT FROM ccursor_customer INTO @customerId,@fname,@city
 while @@FETCH_STATUS = 0 
-	begin 
-		FETCH NEXT FROM ccursor_customer INTO @customerId,@fname,@city
+	begin 		
 		print 'customer: ' + cast(@customerid as varchar)+' => '+@fname+' from '+@city; 
+		FETCH NEXT FROM ccursor_customer INTO @customerId,@fname,@city
 
 	end 
 
